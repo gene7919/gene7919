c3d3562c2574d7c641fb6614ba10658244a403a8
Marco Antonio Saverio Mazzitelli <43501360+gene7919@users.noreply.github.com>
2026-03-16 23:11:32 +0100

    Update package version and add dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="gene7919",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "web3>=6.0.0",
+        "python-dotenv>=1.0.0",
+    ],
 )
